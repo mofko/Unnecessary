0f109db91dfb9f1a78955b8da56260020119d7bb
MOFKO <forimok.com@gmail.com>
2025-09-09 23:44:17 +0300

    AllInOne.py

--- AllInOne.py
+++ AllInOne.py
@@ -1,40 +1,40 @@
 # meta developer: @mofkomodules
 # name: AllInOne
-
-__version__ = (1, 0, 1)
+version = (1, 0, 1)
 
 from telethon import types
 from .. import loader, utils
 
+
 @loader.tds
 class AllInOne(loader.Module):
     """Интерактивный модуль для чата!"""
+
     strings = {"name": "AllInOne"}
 
     async def sborcmd(self, message):
         """<текст> - Общий сбор."""
         args = utils.get_args_raw(message)
         chat = await message.get_chat()
-
         if not isinstance(chat, (types.Chat, types.Channel)):
             await message.edit("<b>🚫 Это не чат!</b>")
             return
 
         await message.edit("<b>Оповещаю...</b>")
-        participants = await message.client.get_participants(chat)
 
-        mentions = []
-        for user in participants:
-            if user.bot:
-                continue
-            mentions.append(f"<a href='tg://user?id={user.id}'>{user.first_name}</a>")
+        participants = await message.client.get_participants(chat)
+        
+        mentions = [
+            f"<a href='tg://user?id={user.id}'>{user.full_name or user.username or str(user.id)}</a>"
+            for user in participants
+            if not user.bot
+        ]
 
         if not mentions:
             await message.edit("<b>🚫 У вас тут пусто...</b>")
             return
 
         text = args + "\n\n" if args else ""
-        text += "<tg-spoiler>" + " ".join(mentions) + "</tg-spoiler>"
-
+        text += " ".join(mentions)
         await message.respond(text, parse_mode="HTML")
         await message.delete()
